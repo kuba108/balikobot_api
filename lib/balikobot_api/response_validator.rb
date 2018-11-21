f05d7d8802328b03c10260ff4d5fdd294c2ca888
Kuba Malina <majak108@gmail.com>
2018-11-21 14:32:45 +0100

    Solves tricky condition.

--- lib/balikobot_api/response_validator.rb
+++ lib/balikobot_api/response_validator.rb
@@ -2,11 +2,13 @@ module BalikobotApi
   class ResponseValidator
 
     def validate_response(response)
-      unless (200..299).exclude?(response.response_status)
+      if (200..299).include?(response.response_status)
+        true
+      else
+        raise "Error in progress!"
         # response.body.first.each do |response_item|
         #   response_item['errors']
         # end
-        raise "Error in progress!"
       end
     end
 
